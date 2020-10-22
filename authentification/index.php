5b630c33f540882bcd6a457529df58a365b7fdf1
bassirou ngom <bassirou.ngom@gitechsn.com>
2020-10-22 12:33:33 +0000

    authentification project

---
+++ authentification/index.php
@@ -0,0 +1,20 @@
+<?php include "src/include/header.php"; ?>
+<div class="container">
+
+<div class="row" style="text-align: center; border: 1px solid;">  
+    <h2>header</h2>
+</div>
+<div class="row"  style="text-align: center; border: 1px solid;">
+    <div class="col-md-3 col-lg-4 col-xs-1"  style="text-align: center; border: 1px solid red;">
+        <h2>Menu</h2>
+    </div>
+    <div class="col-md-9 col-lg-8 col-xs-11"  style="text-align: center; border: 1px solid red;">
+        <div class="row" style="text-align: center; border: 1px solid green;"> nav</div>
+        <div class="row" style="text-align: center; border: 1px solid yellow;">containre</div>
+    </div>
+</div>
+<div class="row" style="text-align: center; border: 1px solid;">  
+    <h2>Footer</h2>
+</div>
+
+<?php include "src/include/footer.php"; ?>
---
+++ authentification/src/include/footer.php
@@ -0,0 +1,4 @@
+
+   <script src="dist/js/bootstrap.js"></script> 
+</body>
+</html>
